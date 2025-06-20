291df1d7969f31afea1b55adee0d591880f738ca
shimmingyu <alsrb60@petpuls.net>
2025-06-20 23:45:19 +0900

    google analytics 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2DJPKVV3H2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2DJPKVV3H2');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
